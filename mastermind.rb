ec72477f92c2d0afd80c44cbe61406feef4d67ed
Roger Massana <rogermassana2002@gmail.com>
2022-08-08 21:49:37 +0200

    Calculate 4^6 combinations for algorithm

--- mastermind.rb
+++ mastermind.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
-class Game
-end
-
 # Class to play the game with the colors selected by the computer
-class Computer
+class Game
   def initialize(colors)
+    @colors = Marshal.load(Marshal.dump(colors))
     puts 'colors:'
     p colors
     @correct_seq = [colors.delete_at(rand(6)), colors.delete_at(rand(5)), colors.delete_at(rand(4)),
@@ -13,20 +11,8 @@ class Computer
     @remaining_turns = 12
   end
 
-  def play_game
-    while @remaining_turns.positive?
-      break if play_round
 
-      puts "Remaining turns: #{@remaining_turns}"
-    end
-    if @remaining_turns.zero?
-      puts 'You lost!'
-    else
-      puts "Congradulations, you won with #{@remaining_turns} remaining turns!"
-    end
-  end
-
-  private
+  protected 
 
   def decrement_turn
     @remaining_turns -= 1
@@ -38,6 +24,26 @@ class Computer
 
   def colors_outplace(colors)
     @correct_seq.filter { |color| colors.include?(color) }.length
+  end
+
+end
+
+class Player < Game
+
+  def initialize(colors)
+    super(colors)
+  end
+
+  def play_game
+    while @remaining_turns.positive?
+      break if play_round_player
+      puts "Remaining turns: #{@remaining_turns}"
+    end
+    if @remaining_turns.zero?
+      puts 'You lost!'
+    else
+      puts "Congradulations, you won with #{@remaining_turns} remaining turns!"
+    end
   end
 
   def play_round
@@ -53,7 +59,61 @@ class Computer
   end
 end
 
-class Player
+class Computer < Game
+
+  def initialize(colors)
+    super(colors)
+  end
+
+  def play_game
+#    while @remaining_turns.positive?
+ #     break if play_round
+  #    puts "Remaining turns: #{@remaining_turns}"
+  #  end
+  #  if @remaining_turns.zero?
+  #    puts 'You lost!'
+  #  else
+  #    puts "Congradulations, you won with #{@remaining_turns} remaining turns!"
+  #  end
+    play_round
+  end
+  
+  private
+
+  def play_round
+    #in_place = colors_inplace(colors)
+    #out_place = colors_outplace(colors) - in_place
+    #in_place == 4
+    set = calc_combinations
+    puts set.length
+  end
+
+  def calc_combinations
+    set = []
+    i = 1; j = 1; k = 1; l = 1
+    while i <= 6
+      while j <= 6
+        while k <= 6
+          while l <= 6
+            set.push([i, j, k, l])
+            l += 1
+          end
+          l = 1
+          k += 1
+        end
+        k = 1
+        j += 1
+      end
+      j = 1
+      i += 1
+    end
+    set
+  end
+
+  def algotithm
+    
+  end
+
 end
 
 colors = %w[red green blue yellow orange black]
