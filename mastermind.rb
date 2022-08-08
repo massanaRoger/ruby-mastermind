dd98a336627e4ac21e468708df65b77fabf7850d
Roger Massana <rogermassana2002@gmail.com>
2022-08-08 15:03:38 +0200

    Display correct guesses

--- mastermind.rb
+++ mastermind.rb
@@ -6,8 +6,21 @@ end
 
 class Computer
   def initialize(colors)
+    puts "colors:"
+    p colors
     @correct_seq = [colors.delete_at(rand(6)), colors.delete_at(rand(5)), colors.delete_at(rand(4)), colors.delete_at(rand(3))]
+    @remaining_turns = 12
+  end
+
+  def play_round
+    puts 'Select the 4 colors'
+    colors = gets.chomp.split(" ")
+    in_place = @correct_seq.filter.with_index {|color, index| color == colors[index]}.length
+    out_place = @correct_seq.filter{|color| colors.include?(color)}.length - in_place
+    puts "You have guessed #{in_place} number of colors"
+    puts "You have guessed #{out_place} number of colors"
     p @correct_seq
+    @remaining_turns -= 1
   end
 end
 
@@ -17,3 +30,4 @@ end
 
 colors = ["red", "green", "blue", "yellow", "orange", "black"]
 computer = Computer.new(colors)
+computer.play_round
